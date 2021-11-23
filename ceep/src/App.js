068cfe4bdff86943b6048c849c1c0df8a98f7442
Gustavo Pertile Follador <gu.follador@gmail.com>
2021-11-23 20:04:51 -0300

    adicionando pasta do projeto de react da alura

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ListaDeNotas from './components/ListaDeNotas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua nota..."></textarea>
+        <button>Criar nota</button>
+      </form>
+      <ListaDeNotas />
+    </section>
   );
 }
 
---
+++ ceep/src/components/ListaDeNotas.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Component } from "react";
+
+export default class ListaDeNotas extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+    );
+  }
+}
