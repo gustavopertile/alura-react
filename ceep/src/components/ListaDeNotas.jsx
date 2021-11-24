aaec3ff7101979081ec8c0d4d61c2707ffa06272
Gustavo Pertile Follador <gu.follador@gmail.com>
2021-11-24 16:32:30 -0300

    adicionando pasta do curso de react da alura

--- ceep/src/components/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Component } from "react";
-import CardNota from "./CardNota";
+import CardNota from "./CardNota/CardNota";
 
 class ListaDeNotas extends Component {
   render() {
     return (
       <ul>
         {console.log("Aqui é uma tag JS")}
-        {Array.of("Trabalho", "Trabalho", "Estudos").map(categoria => {
+        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria,index) => {
           return (
-            <li>
-              <div>{categoria}</div>
+            <li key={index}>
               <CardNota />
             </li>
           )
