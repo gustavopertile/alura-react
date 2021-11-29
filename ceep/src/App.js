e034044d5c9b8a1af9f269ec4aef47707d6909fa
Gustavo Pertile Follador <gu.follador@gmail.com>
2021-11-29 19:44:50 -0300

    adicionando pasta do curso de  react da alura

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,18 +1,36 @@
-import React from 'react'
-import { Component } from 'react';
-import  FormularioCadastro from './components/FormularioCadastro';
-import ListaDeNotas from './components/ListaDeNotas';
-
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
   render() {
     return (
-      <section>
-        <FormularioCadastro />
-        <ListaDeNotas />
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
-
 }
 
+//new ListaDeNotas({notas:this.notas})
 export default App;
--- ceep/src/components/CardNota/CardNota.jsx
+++ ceep/src/components/CardNota/CardNota.jsx
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import "./estilo.css"
-
+import React, { Component } from "react";
+import "./estilo.css";
 class CardNota extends Component {
-    render() {
-        return (
-            <section className="card-nota">
-                <header>
-                    <h3>Título</h3>
-                </header>
-                <p>Escreva sua nota</p>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section className="card-nota">
+        <header className="card-nota_cabecalho">
+          <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+        </header>
+        <p className="card-nota_texto">{this.props.texto}</p>
+      </section>
+    );
+  }
 }
 
 export default CardNota;
