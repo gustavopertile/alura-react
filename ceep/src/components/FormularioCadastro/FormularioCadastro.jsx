3d4712a41f00c47c6cc8ea303a7b290e388721e2
Gustavo Pertile Follador <gu.follador@gmail.com>
2021-12-03 18:07:38 -0300

    adicionando pasta de react do curso da alura

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,27 +2,48 @@ import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.titulo ="";
-    this.texto ="";
+    this.titulo = "";
+    this.texto = "";
+    this.categoria = "Sem categoria";
+    this.state = { categorias: [] }
+    
+    this._novasCategorias = this._novasCategorias.bind(this)
   }
 
-  _handleMudancaTitulo(evento){
+  componentDidMount() {
+    this.props.categorias.inscrever(this._novasCategorias)
+    
+  }
+
+  componentWillUnmount() {
+    this.props.categorias.desinscrever(this._novasCategorias)
+  }
+
+  _novasCategorias(categorias) {
+    this.setState({ ...this.state, categorias })
+  }
+
+  _handleMudancaCategoria(evento) {
+    evento.stopPropagation();
+    this.categoria = evento.target.value;
+  }
+
+  _handleMudancaTitulo(evento) {
     evento.stopPropagation();
     this.titulo = evento.target.value;
   }
 
-  _handleMudancaTexto(evento){
+  _handleMudancaTexto(evento) {
     evento.stopPropagation();
     this.texto = evento.target.value;
   }
 
-  _criarNota(evento){
+  _criarNota(evento) {
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.texto);
-    
+    this.props.criarNota(this.titulo, this.texto, this.categoria);
   }
 
   render() {
@@ -30,6 +51,13 @@ class FormularioCadastro extends Component {
       <form className="form-cadastro"
         onSubmit={this._criarNota.bind(this)}
       >
+        <select onChange={this._handleMudancaCategoria.bind(this)}
+          className="form-cadastro_input">
+          <option>Sem Categoria</option>
+          {this.state.categorias.map((categoria, index) => {
+            return <option key={index}>{categoria}</option>
+          })}
+        </select>
         <input
           type="text"
           placeholder="Título"
